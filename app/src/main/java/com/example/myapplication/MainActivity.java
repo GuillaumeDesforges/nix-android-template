d4dd2ee20cd73a550ebc1926374eab9edcc5f7bd
Guillaume Desforges <aceus02@gmail.com>
2020-11-28 17:08:51 +0100

    Fix app

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,6 +7,5 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
